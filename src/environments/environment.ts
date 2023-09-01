edcf2eb2791ba6b639698c2b7a6e4a7bedf9b49f
core.editor <an.munozq@duocuc.cl>
2023-08-31 23:53:09 -0400

    estructura básica de login y signup funcional con Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'login-firebase-angular-16',
+    appId: '1:274037204450:web:df2d7d1200a2de065b05bb',
+    storageBucket: 'login-firebase-angular-16.appspot.com',
+    apiKey: 'AIzaSyAh9CJ6zKTVYZKZB1K1W6yhUEeAywlvFcg',
+    authDomain: 'login-firebase-angular-16.firebaseapp.com',
+    messagingSenderId: '274037204450',
+  },
+  apiUrl:'',
+  production: false,
+};
